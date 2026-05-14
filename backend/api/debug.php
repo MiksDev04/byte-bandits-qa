75b09765678fcf53b00eb275be9ecc86b548abea
MiksDev04 <mikogapasan04@gmail.com>
2026-05-14 13:28:54 +0800

    asdsd

--- backend/api/debug.php
+++ backend/api/debug.php
@@ -1,13 +1,6 @@
 <?php
-$autoload = __DIR__ . '/../../vendor/autoload.php';
-require_once $autoload;
-
-$psr4File = __DIR__ . '/../../vendor/composer/autoload_psr4.php';
-$psr4 = file_exists($psr4File) ? require $psr4File : [];
-
+$srcPath = __DIR__ . '/../../vendor/phpmailer/phpmailer/src';
 echo json_encode([
-    'phpmailer_installed'    => class_exists('PHPMailer\PHPMailer\PHPMailer'),
-    'phpmailer_dir_contents' => scandir(__DIR__ . '/../../vendor/phpmailer'),
-    'psr4_map'               => $psr4,
-    'autoload_psr4_exists'   => file_exists($psr4File),
+    'src_exists'   => is_dir($srcPath),
+    'src_contents' => is_dir($srcPath) ? scandir($srcPath) : [],
 ]);
