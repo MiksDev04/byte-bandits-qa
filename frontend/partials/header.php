1ac33459616589480dac06fb4aea71d469da61b6
MiksDev04 <mikogapasan04@gmail.com>
2026-05-15 10:22:04 +0800

    sdfds

--- frontend/partials/header.php
+++ frontend/partials/header.php
@@ -20,6 +20,10 @@ $userInitials = strtoupper(
 $userRole = htmlspecialchars($_SESSION['role'] ?? 'viewer');
 ?>
 
+<head>
+  <link rel="shortcut icon" href="../assets/images/byte-bandits-qa.ico" type="image/x-icon">
+</head>
+
 <header class="qa-header">
 
   <!-- Mobile menu toggle -->
